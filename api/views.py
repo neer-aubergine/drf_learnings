3a02ced3ca0119f07932307e2cc7a7b47363217f
neer-aubergine <neer@auberginesolutions.com>
2025-01-09 15:44:10 +0530

    added nested serializers

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from api.serializers import productSerializer
-from api.models import Product
+from api.serializers import productSerializer , orderSerializer , orderItemSerializer
+from api.models import Product , Order , OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -21,4 +21,10 @@ def product_list(request):
 def product_detail(request , pk):
     products = get_object_or_404(Product , pk=pk)
     serializer = productSerializer(products)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = orderSerializer(orders, many=True)
     return Response(serializer.data)
