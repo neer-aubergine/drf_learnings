4e105abdf66684d9309226afc29a9ace2753d013
neer-aubergine <neer@auberginesolutions.com>
2025-01-09 10:26:07 +0530

    added models

---
+++ api/models.py
@@ -0,0 +1,51 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    stock = models.PositiveIntegerField()
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    class statusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+
+    order_id = models.UUIDField(primary_key=True, editable=False , default=uuid.uuid4)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20, 
+        choices=statusChoices.choices, 
+        default=statusChoices.PENDING
+        )
+    products = models.ManyToManyField(Product, through='OrderItem' , related_name='orders')
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in order {self.order.order_id}"
